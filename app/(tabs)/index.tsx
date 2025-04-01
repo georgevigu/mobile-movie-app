afcaa1c2f222c387777cf149ac94b52a502dc3dc
George Vigu <vigu.george@gmail.com>
2025-04-01 14:47:52 +0300

    Added movie details page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,13 +9,21 @@ import {
 	FlatList,
 } from "react-native";
 import SearchBar from "@/components/searchBar";
+import TrendingCard from "@/components/trendingCard";
 import { useRouter } from "expo-router";
 import useFetch from "../../services/useFetch";
 import { fetchMovies } from "../../services/api";
 import MovieCard from "@/components/movieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
 export default function Index() {
 	const router = useRouter();
+
+	const {
+		data: trendingMovies,
+		loading: trendingLoading,
+		error: trendingError,
+	} = useFetch(getTrendingMovies);
 
 	const {
 		data: movies,
@@ -33,14 +41,14 @@ export default function Index() {
 			>
 				<Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-				{moviesLoading ? (
+				{moviesLoading || trendingLoading ? (
 					<ActivityIndicator
 						size="large"
 						color="#0000ff"
 						className="mt-10 self-center"
 					/>
-				) : moviesError ? (
-					<Text>Error: {moviesError?.message}</Text>
+				) : moviesError || trendingError ? (
+					<Text>Error: {moviesError?.message || trendingError?.message}</Text>
 				) : (
 					<View className="flex-1 mt-5">
 						<SearchBar
@@ -49,10 +57,27 @@ export default function Index() {
 							value=""
 							onChangeText={() => {}}
 						/>
+						{trendingMovies && (
+							<View className="mt-10">
+								<Text className="text-lg text-white font-bold mb-3">
+									Trending Movies
+								</Text>
+								<FlatList
+									horizontal
+									showsHorizontalScrollIndicator={false}
+									ItemSeparatorComponent={() => <View className="w-4" />}
+									className="mb-4 mt-3"
+									data={trendingMovies}
+									renderItem={({ item, index }) => <TrendingCard movie={item} index={index}/>}
+									keyExtractor={(item) => item.movie_id.toString()}
+								/>
+							</View>
+						)}
 						<>
 							<Text className="text-lg text-white font-bold mt-5 mb-3">
 								Latest Movies
 							</Text>
+
 							<FlatList
 								data={movies}
 								renderItem={({ item }) => <MovieCard {...item} />}
--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -34,11 +34,6 @@ const MovieCard = ({
 					<Text className="text-xs text-light-300 font-medium mt-1">
 						{release_date?.split("-")[0]}
 					</Text>
-					<Text>
-						{/* <Text className="text-xs font-medium text-light-300 uppercase">
-							Movie
-						</Text> */}
-					</Text>
 				</View>
 			</TouchableOpacity>
 		</Link>
